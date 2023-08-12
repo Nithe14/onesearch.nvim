1cc7d9b64b629eb13ffd3392b2ed7a69b9a262b5
Nithe14 <imnithe@gmail.com>
2023-08-12 19:46:40 +0200

    testing

--- lua/onesearch/init.lua
+++ lua/onesearch/init.lua
@@ -386,7 +386,8 @@ local function search()
 
 	elseif key == M.K_UpArrow then -- show last searched pattern
 	    search_index = search_index - 1 
-            pattern = vim.fn.histget("search", search_index) or ""
+	    pattern = vim.fn.histget("search")
+            --pattern = vim.fn.histget("search", search_index) or ""
 
         elseif key == M.K_DownArrow then -- show first searched pattern
 	    search_index = search_index + 1
