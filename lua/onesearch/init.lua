2df0f49d4e230aeaddaf24c5251b47106a661f88
Nithe14 <imnithe@gmail.com>
2023-08-12 19:06:47 +0200

    testing

--- lua/onesearch/init.lua
+++ lua/onesearch/init.lua
@@ -374,8 +374,8 @@ local current_search_index = 0
 
 local function search()
     if M.conf.save_search_history then
-	    --last_searched_pattern = vim.fn.getreg('/')
-	    last_searched_pattern = load_search_history(M.conf.search_history_file_path)
+	    last_searched_pattern = vim.fn.getreg('/')
+	    --last_searched_pattern = load_search_history(M.conf.search_history_file_path)
     else
     	last_searched_pattern = {}
     end
@@ -405,7 +405,7 @@ local function search()
 	    		current_search_index = #last_searched_pattern
 		end
 		if M.conf.save_search_history then
-			vim.fn.setreg('/', load_search_history)
+			--vim.fn.setreg('/', load_search_history)
 			save_search_history(last_searched_pattern, M.conf.search_history_file_path)
 		end
             break -- accept
